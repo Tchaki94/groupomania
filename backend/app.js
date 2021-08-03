b1ab2f087f55e3cbce47e0636d4ac63b939375d8
guillaume L <mister941@msn.com>
2021-08-03 11:07:06 +0200

    mise en place du frontend

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const path = require('path');
+const helmet = require('helmet');
+
 
 const app = express();
 
@@ -11,3 +14,9 @@ app.use((req, res, next) => {
 
 
 app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(helmet());
+
+module.exports = app;
